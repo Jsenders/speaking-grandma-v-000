bba88e030a195047f8f3ef258617dbedf919585b
Joseph Senders <jasenders@gmail.com>
2018-06-06 21:55:27 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -2,9 +2,9 @@
 def speak_to_grandma(phrase)
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
-if phrase == phrase.upcase 
+if phrase == phrase.upcase
 "NO, NOT SINCE 1938!"
-else 
+else
 "HUH?! SPEAK UP, SONNY!"
 # unless you shout it (type in all capitals).
 # If you shout, she can hear you (or at least she thinks so)
